5116d6d062a36e8f2da4b01d1ce3a775bea1f6a6
procfs <procfs@procfss-MacBook-Pro.local>
2019-12-29 21:13:20 +0900

    adjusted build scripts

--- linux-build.sh
+++ linux-build.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
+aclocal
+automake --add-missing
 autoconf
-automake
 CXX=clang++ CXXFLAGS="-g -O3 $(llvm-config --cxxflags) $(llvm-config --ldflags) $(llvm-config  --system-libs) $(llvm-config --libs core)" ./configure
 make
